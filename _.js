b3e1ee9618815e9d41f05746b6a41f744c3cbe3e
musa şahin kundakcı <musa.20@windowslive.com>
2022-06-19 17:46:25 +0300

    New functions.

--- _.js
+++ _.js
@@ -1,5 +1,10 @@
 function cu(target) {
     let ptr = document.querySelector(target);
+
+    /**
+     *
+     * @defination Basic html operations
+     */
     ptr.text = function (text) {
         if (text) {
             ptr.innerText = text;
@@ -16,17 +21,54 @@ function cu(target) {
         }
         return ptr;
     };
+
+    /**
+     * @defination Bind event with the living element
+     * @param {*} eventName
+     * @param {*} callback
+     * @returns ptr
+     */
     ptr.on = function (eventName, callback) {
         ptr.addEventListener(eventName, callback);
         return ptr;
     };
+
+    /**
+     * @defination  Bind event with the living element
+     * @param {*} callback
+     * @returns ptr
+     */
     ptr.click = function (callback) {
         ptr.addEventListener('click', callback);
         return ptr;
     };
-    ptr.dblclick(callback){
-        ptr.addEventListener("dblclick",callback)
+    ptr.dblclick = function (callback) {
+        ptr.addEventListener('dblclick', callback);
         return ptr;
+    };
+    ptr.hover = function (callback) {
+        ptr.addEventListener('mouseover', callback);
+        return ptr;
+    };
+    ptr.mouseEnter = function (callback) {
+        ptr.addEventListener('mouseenter', callback);
+        return ptr;
+    };
+    ptr.mouseLeave = function (callback) {
+        ptr.addEventListener('mouseleave', callback);
+        return ptr;
+    };
+    ptr.toggle = function () {
+        ptr.css({
+            display: ptr.style.display === 'none' ? 'inline-block' : 'none',
+        });
+    };
+
+    /**
+     * @defination AJAX operations
+     */
+    async function get(url) {
+        fetch();
     }
     return ptr;
 }
